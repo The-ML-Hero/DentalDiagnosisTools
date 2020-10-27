fe01ac916fd56819acdb9a2b3ede772cbb771a1c
The-ML-Hero <67855775+The-ML-Hero@users.noreply.github.com>
2020-10-27 19:19:01 +0530

    Update root_seg.py

--- root_seg.py
+++ root_seg.py
@@ -39,6 +39,8 @@ def write():
     )
   uploaded_file_img = st.file_uploader("Choose an Input Image", type="png",accept_multiple_files=False)
   if uploaded_file_img is not None:
+    url_mask = 'https://blob-ap-south-1-ukyez4.s3.ap-south-1.amazonaws.com/sara/c6/c668/c668fa65-a0ec-4905-9d9a-341368dc3bb9.bin?response-content-disposition=attachment%3B%20filename%3D%22MASK_RCNN_ROOT_SEGMENTATION.pth%22&response-content-type=&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAI75SICYCOZ7DPWTA%2F20201027%2Fap-south-1%2Fs3%2Faws4_request&X-Amz-Date=20201027T125742Z&X-Amz-SignedHeaders=host&X-Amz-Expires=1800&X-Amz-Signature=2592ecfbd1f7de226c77b15782d1f82a852115fe6602e80b1ddbdd8406bce030'
+    requests.get(url_mask ,stream=True)    
     file_random = secrets.token_hex(4)
     o = int(np.random.randint(low=10301319,high=9987869996)) 
     cfg = get_cfg()
