9ce7f87a35311410bd9670bb3b19234bf89d639d
The-ML-Hero <67855775+The-ML-Hero@users.noreply.github.com>
2020-10-28 21:38:17 +0530

    Update root_seg.py

--- root_seg.py
+++ root_seg.py
@@ -34,7 +34,7 @@ class RootDetector(Document):
 @st.cache
 def write():
 
-  if true:    
+  if True:    
       st.set_option('deprecation.showfileUploaderEncoding', False)
       st.title('Root Anatomy Prediction by A.Adithya Sherwood IX-E')
       st.subheader('Disclaimer: Please check with your local specialized dentist, if you are in doubt please try atleast twice.')
