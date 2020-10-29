c85e01f892d3e3e5f3766ba7b002c4f93d99a55b
The-ML-Hero <67855775+The-ML-Hero@users.noreply.github.com>
2020-10-29 13:25:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,17 +5,17 @@ import streamlit as st
 import streamlit.components.v1 as stc
 import tooth_reconstruction
 import display_app as udisp
-#import Pulp_Segmentation
+import Pulp_Segmentation
 import root_seg
 import fracture_tooth_detection
 import flurosis_tooth_detection
 
-
+os.system("git clone https://gitlab.com/sherwoodadithya/dentaldiagnosistoolkitai.git")
 
 MENU = {
     
     "Fracture Tooth Detection" : fracture_tooth_detection,
-    #"Root Anatomy Prediction" : root_seg,
+    "Root Anatomy Prediction" : root_seg,
     "Flurosis Detection" : flurosis_tooth_detection,	
     "Tooth Fracture Reconstruction" : tooth_reconstruction,	
 }
