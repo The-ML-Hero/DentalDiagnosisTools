7522e0d39212789c590ab3b2d891267237fef2ef
The-ML-Hero <67855775+The-ML-Hero@users.noreply.github.com>
2020-11-01 22:18:11 +0530

    Update download_page.py

--- download_page.py
+++ download_page.py
@@ -12,3 +12,4 @@ def write():
     st.subheader('Available In Android,Windows,MacOS')
     st.subheader('Linux Comming Soon!')
     stc.html("""<iframe frameborder="0" src="https://itch.io/embed/807153?linkback=true&amp;border_width=5" width="560" height="175"><a href="https://the-ml-hero.itch.io/dental-diagnosis">Dental Diagnosis by The-ML-Hero</a></iframe>""")
+    st.warning("I Recommend That You Use The WebApp Version Of This Project Since You Get Almost All Features Of the Native Build")
