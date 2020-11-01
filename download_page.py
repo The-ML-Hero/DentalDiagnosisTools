77a54cb3acfe3439e702c52caf6d9da91e02e8ec
The-ML-Hero <67855775+The-ML-Hero@users.noreply.github.com>
2020-11-01 16:52:27 +0530

    Create download_page.py

---
+++ download_page.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import os
+import PIL
+import cv2
+import secrets
+import streamlit.components.v1 as stc
+
+
+
+def write():
+    st.title("Made By A.Adithya Sherwood IX-E")
+    st.subheader('Available In Android,Windows,MacOS')
+    st.subheader('Linux Comming Soon!')
+    st.html("""<iframe frameborder="0" src="https://itch.io/embed/807153?linkback=true&amp;border_width=5" width="560" height="175"><a href="https://the-ml-hero.itch.io/dental-diagnosis">Dental Diagnosis by The-ML-Hero</a></iframe>""")
