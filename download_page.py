1c346086c67dfa3bd06d4f11979e47f3147c6849
The-ML-Hero <67855775+The-ML-Hero@users.noreply.github.com>
2020-11-01 16:55:24 +0530

    Update download_page.py

--- download_page.py
+++ download_page.py
@@ -11,4 +11,4 @@ def write():
     st.title("Made By A.Adithya Sherwood IX-E")
     st.subheader('Available In Android,Windows,MacOS')
     st.subheader('Linux Comming Soon!')
-    stc.html("""<iframe frameborder="0" src="https://itch.io/embed/807153?linkback=true&amp;border_width=5" width="560" height="215"><a href="https://the-ml-hero.itch.io/dental-diagnosis">Dental Diagnosis by The-ML-Hero</a></iframe>""")
+    stc.html("""<iframe frameborder="0" src="https://itch.io/embed/807153?linkback=true&amp;border_width=5" width="560" height="175"><a href="https://the-ml-hero.itch.io/dental-diagnosis">Dental Diagnosis by The-ML-Hero</a></iframe>""")
