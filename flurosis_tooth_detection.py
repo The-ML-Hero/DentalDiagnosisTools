f2e5e530bb3e53165ba3d31c835ae098e39c4c95
The-ML-Hero <67855775+The-ML-Hero@users.noreply.github.com>
2021-05-18 13:49:34 +0530

    Update flurosis_tooth_detection.py

--- flurosis_tooth_detection.py
+++ flurosis_tooth_detection.py
@@ -4,19 +4,7 @@ import PIL
 import cv2
 import secrets
 import streamlit.components.v1 as stc
-from mongoengine import Document
-from mongoengine import FileField
-from mongoengine import *
 
-connect(host="mongodb+srv://adithya_admin:YlV1pML2mjkP2LMb@dental-diagnosis.xevq3.mongodb.net/dental_diagnosis?retryWrites=true&w=majority")
-
-class FlurosisDetector(Document):
-    # by default collection_name is 'fs'
-    # you can use collection_name parameter to set the name of the collection
-    # use collection_name like this: FileField(collection_name='images')
-    _id = StringField(required=True)
-    description = StringField(required=True)
-    image_fluro = FileField()
 
 def write():
     st.set_option('deprecation.showfileUploaderEncoding', False)
@@ -31,18 +19,10 @@ def write():
         image = PIL.Image.open(uploaded_file)
         image = image.resize((416,416))
         image.save(f'./Test_Flurosis{file_random}.jpg')
-
         image_flurosis = open(f'./Test_Flurosis{file_random}.jpg','rb')
-        flurosis_form = FlurosisDetector(_id = secrets.token_hex(4),description='Uploaded Flurosis Image',image_fluro=image_flurosis)
-        flurosis_form.save()
-
         st.image(image, caption='Uploaded Image.', use_column_width=True)
         st.write("")
         os.system(f"python3 detect.py --weights './weights/best (2).pt' --img 416 --conf {str(conf_score)} --source ./Test_Flurosis{file_random}.jpg --output ./inference/output")
         image_pred = PIL.Image.open(f'./inference/output/Test_Flurosis{file_random}.jpg')
-
         image_flurosis_out = open(f'./inference/output/Test_Flurosis{file_random}.jpg','rb')
-        flurosis_form_out = FlurosisDetector(_id = secrets.token_hex(4),description='Predicted Flurosis Image',image_fluro=image_flurosis_out)
-        flurosis_form_out.save()
-
         st.image(image_pred, caption='Predictions.', use_column_width=True)
